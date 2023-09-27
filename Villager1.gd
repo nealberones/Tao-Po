93cf52d3347131ae76d0016691815183ae3d0322
nealberones <neal.berones@obf.ateneo.edu>
2023-09-27 12:27:07 +0800

    update color match game
    added: checking if player cards match house cards
    
    to add: different arranged house cards for each house

--- Villager1.gd
+++ Villager1.gd
@@ -11,3 +11,4 @@ func _ready():
 
 func _process(delta):
 	animatedSprite.play("Idle")
+	
---
+++ game_global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var color_game_running = false
+var house_current_frame
+var player_current_frame
+
